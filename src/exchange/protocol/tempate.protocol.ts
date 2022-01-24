aaef70c346c8e4f475aca5f809478c0437e61863
tomgoguo <tomgoguo@tencent.com>
2022-01-24 16:23:19 +0800

    add ts-interface checker

---
+++ src/exchange/protocol/tempate.protocol.ts
@@ -0,0 +1,11 @@
+interface GoodTempalte {
+  name: string
+  age: number
+  likes: []
+}
+
+interface GoodTempalte2 {
+  name: string
+  age: number
+  dislikes: []
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { createCheckers } from 'ts-interface-checker'
+import tempTI from './exchange/protocol/tempate.protocol-ti'
 export const Greeter = (name: string) => `Hello ${name}`
 const a = 'HH'
 
-class Test { }
+class Test {}
 
 console.log('herelu')
 
 const i: number = 0
+
+const { GoodTempalte } = createCheckers(tempTI)
+console.log(GoodTempalte.check({ age: 1, name: 'Helo', likes: ['a', 'c'] }))
